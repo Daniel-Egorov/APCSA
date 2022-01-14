8ca7e3ada5d8caf3d0374b8f1304a09896bddf1e
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-14 08:41:50 -0800

    complete AsciiArt.java lab

--- Unit-00/src/AsciiArt.java
+++ Unit-00/src/AsciiArt.java
@@ -1,23 +1,32 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 14 Jan 2022 
+//Class - APCSA
+//Lab  - Unit 0a3
 
 public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
-		System.out.println("\n\n\n\n" );
-
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
+		System.out.println("Daniel Egorov \n\n" );
+		System.out.println("Little bug dude");
+		System.out.println("\n" );
+		System.out.println("            \\      /            ");
+		System.out.println("             \\    /             ");
+		System.out.println("              \\  /              ");
+		System.out.println("               \\/               ");
+		System.out.println("             +-----+             ");
+		System.out.println("             | o o |             ");
+		System.out.println("             |  _  |             ");
+		System.out.println("             +-----+             ");
+		System.out.println("           \\   /\\              ");
+		System.out.println("            \\ /  \\__           ");
+		System.out.println("             /    \\ \\          ");
+		System.out.println("            [------] \\          ");
+		System.out.println("            |      |             ");
+		System.out.println("           _|     _|             ");
 		//add other output
 
 		System.out.println(" \n\n\n\nHelpFul Hints" );
