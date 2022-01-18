6f092610093089901724678f1414c54a2fd0b2be
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-18 14:55:42 -0800

    complete NumberVerify.java lab

--- Unit-04/src/NumberVerify.java
+++ Unit-04/src/NumberVerify.java
@@ -1,21 +1,19 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -  
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date -  18 Jan 2022
+//Class - APCSA
+//Lab  - Unit 4 NumberVerify
 
 import static java.lang.System.*;
 
 public class NumberVerify
 {
-	public static boolean isOdd( int num )
-	{
-		return true;
+	public static boolean isOdd(int n) {
+		return n % 2 == 1;
 	}
-	public static boolean isEven( int num )
-	{
-		return true;
+	public static boolean isEven(int n) {
+		return n % 2 == 0;
 	}	
 }
--- Unit-04/src/NumberVerifyRunner.java
+++ Unit-04/src/NumberVerifyRunner.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date - 
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 18 Jan 2022
+//Class - APCSA
+//Lab  - Unit 4 NumberVerify
 
 import static java.lang.System.*;
 import java.util.Scanner;
@@ -14,9 +14,26 @@ public class NumberVerifyRunner
 	public static void main ( String[] args )
 	{
 		//add in input
+		Scanner keyboard = new Scanner(System.in);
+		
+		
 		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
 		System.out.println("5 is even :: " + NumberVerify.isEven(5));
 		
 		//add in more test cases
+		System.out.print("Enter an integer :: ");
+		int test = keyboard.nextInt();
+		System.out.println(String.format("%d is odd :: %b", test, NumberVerify.isOdd(test)));
+		System.out.println(String.format("%d is even :: %b", test, NumberVerify.isEven(test)));
+		
+		System.out.print("Enter an integer :: ");
+		test = keyboard.nextInt();
+		System.out.println(String.format("%d is odd :: %b", test, NumberVerify.isOdd(test)));
+		System.out.println(String.format("%d is even :: %b", test, NumberVerify.isEven(test)));
+		
+		System.out.print("Enter an integer :: ");
+		test = keyboard.nextInt();
+		System.out.println(String.format("%d is odd :: %b", test, NumberVerify.isOdd(test)));
+		System.out.println(String.format("%d is even :: %b", test, NumberVerify.isEven(test)));
 	}
 }
