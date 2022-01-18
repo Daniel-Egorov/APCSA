8a51827b2e81d229b21515d2ecdadc9828ce0133
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-18 08:58:22 -0800

    complete Cube.java lab

---
+++ Unit-02/src/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Daniel Egorov
+//Date - 18 Jan 2022
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return 6 * Math.pow(side, 2);
+	}
+}
