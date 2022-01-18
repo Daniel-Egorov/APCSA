b56b11d884306e21e8c49465f3618ddebf79b30e
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-18 14:32:29 -0800

    complete AddSubMult.java lab

--- Unit-04/src/AddSubMult.java
+++ Unit-04/src/AddSubMult.java
@@ -1,14 +1,15 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*; 
 import java.util.Scanner;
 
 public class AddSubMult
 {
-	public static double check( double a, double b )
-	{
-		return 0;
+	public static double check( double a, double b ) {
+		if (a > b) return a - b;
+		else if (b > a) return b - a;
+		else return a * b;
 	}
 }
