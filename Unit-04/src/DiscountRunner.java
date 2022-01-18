61321f3b2b705b04499a596b3e2ebd32af44c63c
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-18 14:37:59 -0800

    complete Discount.java lab

--- Unit-04/src/DiscountRunner.java
+++ Unit-04/src/DiscountRunner.java
@@ -18,6 +18,7 @@ public class DiscountRunner
 		out.print("Enter the original bill amount :: ");
 		double amt = keyboard.nextDouble();
 		
-
+		System.out.printf("Bill after discount :: %.2f", Discount.getDiscountedBill(amt));
+		
 	}
 }
