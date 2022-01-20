7d7e859e354a898a719d91483e8b7cc144021bc8
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-20 09:05:42 -0800

    complete FirstLastVowel.java lab

--- Unit-05/src/FirstLastVowel.java
+++ Unit-05/src/FirstLastVowel.java
@@ -1,13 +1,17 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Daniel Egorov
+//Date - 20 Jan 2022
 
 
-public class FirstLastVowel
-{
-   public static String go( String a )
-	{
-		return "add code here";
+public class FirstLastVowel {
+	public static String go( String a ) {
+		String vowels = "aeiouAEIOU";
+		String first = a.substring(0, 1);
+		String last = a.substring(a.length() - 1);
+		
+		if (vowels.contains(first) || vowels.contains(last)) 
+			return "yes";
+		return "no";
 	}
 }
--- Unit-05/src/VowelsRunner.java
+++ Unit-05/src/VowelsRunner.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Daniel Egorov
+//Date - 20 Jan 2022
 
 public class VowelsRunner
 {
