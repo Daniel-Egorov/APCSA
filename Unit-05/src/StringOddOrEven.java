34db334dc5098f24ced4e33cb37f9d8a21277aff
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-20 14:18:16 -0800

    complete StringOddOrEven.java lab

--- Unit-05/src/StringOddOrEven.java
+++ Unit-05/src/StringOddOrEven.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date - 
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 20 Jan 2022
+//Class - APCSA
+//Lab  - Unit 5 StringOddOrEven
 
 import static java.lang.System.*;
 import java.util.Scanner;
@@ -13,26 +13,27 @@ public class StringOddOrEven
 {
 	private String word;
 
-	public StringOddOrEven()
-	{
+	public StringOddOrEven() {
 	}
 
-	public StringOddOrEven(String s)
-	{
+	public StringOddOrEven(String s) {
+		this.word = s;
 	}
 
-	public void setString(String s)
-	{
+	public void setString(String s) {
+		this.word = s;
 	}
 
- 	public boolean isEven()
- 	{
-		return false;
+ 	public boolean isEven() {
+ 		return this.word.length() % 2 == 0;
 	}
 
- 	public String toString()
- 	{
- 		String output="";
+ 	public String toString() {
+ 		String result;
+ 		if (this.isEven()) result = "even";
+ 		else result = "odd";
+ 		
+ 		String output = this.word + " is " + result;
  		return output;
 	}
 }
--- Unit-05/src/StringRunner.java
+++ Unit-05/src/StringRunner.java
@@ -1,17 +1,25 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date - 
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 20 Jan 2022
+//Class - APCSA
+//Lab  - Unit 5 StringOddOrEven
 
 import static java.lang.System.*;
 import java.util.Scanner;
 
 public class StringRunner
 {
-	public static void main ( String[] args )
-	{
+	public static void main ( String[] args ) {
+		String word;
+		Scanner keyboard = new Scanner(System.in);
+		StringOddOrEven str = new StringOddOrEven();
+		
+		System.out.print("Enter a word :: ");
+		word = keyboard.next();
+		str.setString(word);
+		System.out.println(str.toString());
+		
 	}
 }
