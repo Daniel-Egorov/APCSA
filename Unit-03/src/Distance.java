7f5b8fe7eb4f7ac52b348117b3c4165804bf2553
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-21 08:42:42 -0800

    update Distance.java lab for more test cases

--- Unit-03/src/Distance.java
+++ Unit-03/src/Distance.java
@@ -18,13 +18,11 @@ public class Distance
 	public Distance()
 	{
 		setCoordinates(0, 0, 0, 0);
-		calcDistance();
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
 		setCoordinates(x1, y1, x2, y2);
-		calcDistance();
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
@@ -33,6 +31,7 @@ public class Distance
 		yOne = y1;
 		xTwo = x2;
 		yTwo = y2;
+		calcDistance();
 	}
 
 	public void calcDistance()
--- Unit-03/src/DistanceRunner.java
+++ Unit-03/src/DistanceRunner.java
@@ -15,22 +15,24 @@ public class DistanceRunner
 	public static void main( String[] args )
 	{
 		Scanner keyboard = new Scanner(System.in);
+		Distance distance = new Distance();
 		
-		System.out.print("Enter X1 :: ");
-		int x1 = keyboard.nextInt();
-		
-		System.out.print("Enter Y1 :: ");
-		int y1 = keyboard.nextInt();
-		
-		System.out.print("Enter X2 :: ");
-		int x2 = keyboard.nextInt();
-		
-		System.out.print("Enter Y2 :: ");
-		int y2 = keyboard.nextInt();
-		
-		//add test cases
-		Distance distance = new Distance(x1, y1, x2, y2);
-		System.out.println(distance.toString());
-		
+		for (int i = 0; i < 5; i++) { // run 5 times
+			System.out.print("Enter X1 :: ");
+			int x1 = keyboard.nextInt();
+			
+			System.out.print("Enter Y1 :: ");
+			int y1 = keyboard.nextInt();
+			
+			System.out.print("Enter X2 :: ");
+			int x2 = keyboard.nextInt();
+			
+			System.out.print("Enter Y2 :: ");
+			int y2 = keyboard.nextInt();
+			
+			//add test cases
+			distance.setCoordinates(x1, y1, x2, y2);
+			System.out.println(distance);
+		}	
 	}
 }
