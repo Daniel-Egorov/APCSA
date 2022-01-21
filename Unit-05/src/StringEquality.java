08fa00c5d04b8f394f919b51a9b0994b0917e2f3
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-21 09:13:41 -0800

    complete StringEquality.java lab

--- Unit-05/src/StringEquality.java
+++ Unit-05/src/StringEquality.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 21 Jan 2022
+//Class - APCSA
+//Lab  - Unit 5 StringEquality
 
 import static java.lang.System.*;
 
@@ -12,25 +12,26 @@ public class StringEquality
 {
 	private String wordOne, wordTwo;
 
-	public StringEquality()
-	{
+	public StringEquality() {
 	}
 
-	public StringEquality(String one, String two)
-	{
+	public StringEquality(String one, String two) {
+		this.setWords(one, two);
 	}
 
-	public void setWords(String one, String two)
-	{
+	public void setWords(String one, String two) {
+		this.wordOne = one;
+		this.wordTwo = two;
 	}
 
-	public boolean checkEquality( )
-	{
-		return false;
+	public boolean checkEquality() {
+		return this.wordOne.equals(this.wordTwo);
 	}
 
-	public String toString()
-	{
-		return wordOne + " does not have the same letters as " + wordTwo + "\n";
+	public String toString() {
+		if (!this.checkEquality())
+			return wordOne + " does not have the same letters as " + wordTwo + "\n";
+		else 
+			return wordOne + " has the same letters as " + wordTwo + "\n";
 	}
 }
--- Unit-05/src/StringEqualityRunner.java
+++ Unit-05/src/StringEqualityRunner.java
@@ -1,18 +1,40 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  - 
+//Name - Daniel Egorov
+//Date - 21 Jan 2022
+//Class - APCSA
+//Lab  - Unit 5 StringEquality
 
 import static java.lang.System.*;
 
-public class StringEqualityRunner
-{
-	public static void main( String args[] )
-	{
+public class StringEqualityRunner {
+	public static void main( String args[] ) {
 		//add test cases
+		StringEquality comparator = new StringEquality();
 		
+		comparator.setWords("hello", "goodbye");
+		System.out.println(comparator);
+		
+		comparator.setWords("one", "two");
+		System.out.println(comparator);
+		
+		comparator.setWords("three", "four");
+		System.out.println(comparator);
+		
+		comparator.setWords("TCEA", "UIL");
+		System.out.println(comparator);
+		
+		comparator.setWords("State", "Champions");
+		System.out.println(comparator);
+		
+		comparator.setWords("ABC", "ABC");
+		System.out.println(comparator);
+		
+		comparator.setWords("ABC", "CBA");
+		System.out.println(comparator);
+		
+		comparator.setWords("Same", "Same");
+		System.out.println(comparator);
 	}
 }
