e718ea72c91c200883de95ef5cb56b2e1990d38d
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-21 08:52:16 -0800

    complete WordsCompare.java lab

--- Unit-05/src/WordsCompare.java
+++ Unit-05/src/WordsCompare.java
@@ -1,43 +1,40 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 21 Jan 2022
+//Class - APCSA
+//Lab  - Unit 5 WordsCompareRunner
 
 import static java.lang.System.*;
 
 public class WordsCompare
 {
 	private String wordOne, wordTwo;
-   private int compare;
+	private int compare;
 
-	public WordsCompare()
-	{
+	public WordsCompare() {
 	}
 
-	public WordsCompare(String one, String two)
-	{
+	public WordsCompare(String one, String two) {
+		this.setWords(one, two);
 	}
 
-	public void setWords(String one, String two)
-	{
-
+	public void setWords(String one, String two) {
+		this.wordOne = one;
+		this.wordTwo = two;
+		this.compare();
 	}
 
-	public void compare()
-	{
+	public void compare() {
+		compare = this.wordOne.compareTo(this.wordTwo);
 	}
 
-	public String toString()
-	{
-		if(compare<0)
-		{
+	public String toString() {
+		if(compare<0) {
 			return wordOne + " should be placed before " + wordTwo + "\n";
 		}
-		else
-		{
+		else {
 			return wordOne + " should be placed after " + wordTwo + "\n";
 		}
 	}
--- Unit-05/src/WordsCompareRunner.java
+++ Unit-05/src/WordsCompareRunner.java
@@ -1,18 +1,39 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Daniel Egorov
+//Date - 21 Jan 2022
+//Class - APCSA
+//Lab  - Unit 5 WordsCompare
 
 import static java.lang.System.*;
 
-public class WordsCompareRunner
-{
-   public static void main( String args[] )
-   {
-		//add test cases
-
-	}
+public class WordsCompareRunner {
+   public static void main( String args[] ) {
+	   WordsCompare comparator = new WordsCompare();
+	   
+	   comparator.setWords("abe", "ape");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("girrafe", "gorilla");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("one", "two");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("fun", "funny");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("123", "19");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("193", "1910");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("goofy", "godfather");
+	   System.out.println(comparator);
+	   
+	   comparator.setWords("funnel", "fun");
+	   System.out.println(comparator);
+   }
 }
