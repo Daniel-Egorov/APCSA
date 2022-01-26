aed6c7947336b1e6082543cbed34a59266d8caf5
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-26 15:28:04 -0800

    add comments

--- Unit-06/src/Perfect.java
+++ Unit-06/src/Perfect.java
@@ -22,8 +22,27 @@ public class Perfect {
   }
   
 	public boolean isPerfect() {
+    /*
+    MAX prevents checking more numbers than necessary,
+    originally it would check all numbers up until the
+    number we are checking, but now it only checks up to
+    the floored integer of the square root of the number
+
+    MAX allows all numbers beyond the square root of 
+    the number to be pre determined as a divisor or not
+
+    take 6 for example, the divisors are 1, 2, 3 (and 6)
+    the floored integer square root of 6 is 2, meaning we 
+    only have to check the numbers 1 and 2. checking 2 we 
+    see that it is a divisor, and from there we can conclude
+    its pair (3) is also a divisor, and because we check only up
+    to 2, we know we dont have to check 4 or 5 because they
+    arent paired with any of the divisors we have already
+    calculated, therefore speeding up the calculation and 
+    especially so for really large numbers
+    */
     int MAX = (int)Math.floor(Math.sqrt(this.number));
-    int divisorSum = 1;
+    int divisorSum = 1; // to store the sum of every divisor 
 
     int i = 2;
     while (i <= MAX) {
@@ -33,7 +52,6 @@ public class Perfect {
       }
       i++;
     }
-    
 		return divisorSum == this.number;
 	}
 
