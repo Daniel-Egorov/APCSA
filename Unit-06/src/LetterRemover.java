1f7ed172f54a60eff54970eead28a6dfc8c16212
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-26 15:42:20 -0800

    complete LetterRemover.java lab

--- Unit-06/src/LetterRemover.java
+++ Unit-06/src/LetterRemover.java
@@ -1,36 +1,41 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*;
 
-public class LetterRemover
-{
+public class LetterRemover {
    private String sentence;
    private char lookFor;
 
-	public LetterRemover()
-	{
+	public LetterRemover() {
 		//call set
+    char c = 0;
+    this.setRemover("", c);
 	}
 
 	//add in second constructor
+	public LetterRemover(String s, char rem) {
+    this.setRemover(s, rem);
+  }
 	
-	
-	public void setRemover(String s, char rem)
-	{
+	public void setRemover(String s, char rem) {
 		sentence = s;
 		lookFor = rem;
 	}
 
-	public String removeLetters()
-	{
-		String cleaned=sentence;
+	public String removeLetters() {
+		String cleaned = sentence;
+
+    while (cleaned.contains("" + this.lookFor)) {
+      cleaned = cleaned.substring(0, cleaned.indexOf("" + this.lookFor)) + 
+                cleaned.substring(cleaned.indexOf("" + this.lookFor) + 1);
+    }
+    
 		return cleaned;
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		return sentence + " - letter to remove " + lookFor;
 	}
 }
--- Unit-06/src/LetterRemoverRunner.java
+++ Unit-06/src/LetterRemoverRunner.java
@@ -1,14 +1,40 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*;
 
-public class LetterRemoverRunner
-{
-	public static void main( String args[] )
-	{
-		//add test cases		
-											
+public class LetterRemoverRunner {
+	public static void main( String args[] ) {
+		//add test cases
+    LetterRemover remover = new LetterRemover();
+
+    remover.setRemover("I am Sam I am", 'a');
+    System.out.println(remover);
+    System.out.println(remover.removeLetters());
+
+    System.out.println();
+
+    remover.setRemover("ssssssssxssssesssssesss", 's');
+    System.out.println(remover);
+    System.out.println(remover.removeLetters());
+
+    System.out.println();
+
+    remover.setRemover("qwertyqwertyqwerty", 'a');
+    System.out.println(remover);
+    System.out.println(remover.removeLetters());
+
+    System.out.println();
+
+    remover.setRemover("abababababa", 'b');
+    System.out.println(remover);
+    System.out.println(remover.removeLetters());
+
+    System.out.println();    
+
+    remover.setRemover("abaababababa", 'x');
+    System.out.println(remover);
+    System.out.println(remover.removeLetters());
 	}
 }
