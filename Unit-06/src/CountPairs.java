9c6e47cd3b2654a7e3c89a14b71ac97875367e1f
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-26 14:15:43 -0800

    complete CountPairs.java lab

--- Unit-06/src/CountPairs.java
+++ Unit-06/src/CountPairs.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name Daniel Egorov
 
 import static java.lang.System.*;
 
@@ -9,6 +9,13 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+    for (int i = 1; i < str.length(); i++) {
+      /*
+      if the character at index i is the same as the 
+      character at index i - 1, increment count by one
+      */
+      if (str.charAt(i) == str.charAt(i - 1)) count++;
+    }
 		return count;
 	}
 }
