af6f2a7e6a2a0172ba07dbe9313362dcf667237c
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-26 15:18:32 -0800

    complete Perfect.java lab

--- Unit-06/src/Perfect.java
+++ Unit-06/src/Perfect.java
@@ -1,22 +1,48 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*;
 
-public class Perfect
-{
-   private int number;
+public class Perfect {
+  private int number;
 
-	//add constructors
+  //add constructors
+  public Perfect() {
+    this.setNum(0);
+  }
+
+  public Perfect(int num) {
+    this.setNum(num);
+  }
 
 	//add a set method
+  public void setNum(int num) {
+    this.number = num;
+  }
+  
+	public boolean isPerfect() {
+    int MAX = (int)Math.floor(Math.sqrt(this.number));
+    int divisorSum = 1;
 
-	public boolean isPerfect()
-	{
-		return false;
+    int i = 2;
+    while (i <= MAX) {
+      if (this.number % i == 0) {
+        divisorSum += i;
+        divisorSum += this.number / i;
+      }
+      i++;
+    }
+    
+		return divisorSum == this.number;
 	}
 
 	//add a toString	
+  public String toString() {
+    if (this.isPerfect())
+      return this.number + " is perfect.";
+    else  
+      return this.number + " is not perfect.";
+  }
 	
 }
--- Unit-06/src/PerfectRunner.java
+++ Unit-06/src/PerfectRunner.java
@@ -1,14 +1,42 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*; 
 
-public class PerfectRunner
-{
-	public static void main( String args[] )
-	{
+public class PerfectRunner {
+	public static void main( String args[] ) {
 		//add test cases
-																
+    Perfect perf = new Perfect();
+
+    perf.setNum(496);
+    System.out.println(perf);
+    
+    perf.setNum(45);
+    System.out.println(perf);
+
+    perf.setNum(6);
+    System.out.println(perf);
+
+    perf.setNum(14);
+    System.out.println(perf);
+
+    perf.setNum(8218);
+    System.out.println(perf);
+
+    perf.setNum(1245);
+    System.out.println(perf);
+
+    perf.setNum(33);
+    System.out.println(perf);
+
+    perf.setNum(28);
+    System.out.println(perf);
+
+    perf.setNum(27);
+    System.out.println(perf);
+
+    perf.setNum(33550336);
+    System.out.println(perf);
 	}
 }
