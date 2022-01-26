9d0b37a6e4049e6979e1864e51f352d72ac0f79d
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-26 14:47:57 -0800

    complete Word.java lab

--- Unit-06/src/Word.java
+++ Unit-06/src/Word.java
@@ -1,43 +1,43 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name Daniel Egorov
 
 import static java.lang.System.*;
 
-public class Word
-{
+public class Word {
 	private String word;
 
-	public Word()
-	{
+	public Word() {
+    this.setString("");
 	}
 
-	public Word(String s)
-	{
+	public Word(String s) {
+    this.setString(s);
 	}
 
-	public void setString(String s)
-	{
+	public void setString(String s) {
+    this.word = s;
 	}
 
-	public char getFirstChar()
-	{
-		return 0;
+	public char getFirstChar() {
+		return this.word.charAt(0);
 	}
 
-	public char getLastChar()
-	{
-		return 0;
+	public char getLastChar() {
+		return this.word.charAt(this.word.length() - 1);
 	}
 
-	public String getBackWards()
-	{
-		String back="";
+	public String getBackWards() {
+		String back = "";
+
+    for (int i = this.word.length() - 1; i > -1; i--) {
+      back += this.word.charAt(i);
+    }
+    
 		return back;
 	}
 
- 	public String toString()
- 	{
- 		return "";
+ 	public String toString() {
+ 		return this.word;
 	}
 }
--- Unit-06/src/WordRunner.java
+++ Unit-06/src/WordRunner.java
@@ -1,13 +1,50 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name Daniel Egorov
 
 import static java.lang.System.*;
 
-public class WordRunner
-{
-	public static void main ( String[] args )
-	{
-		//add test cases		
+public class WordRunner {
+	public static void main ( String[] args ) {
+		//add test cases
+    Word backWord = new Word();
+
+    backWord.setString("Hello");
+    System.out.println(backWord.getFirstChar());
+    System.out.println(backWord.getLastChar());
+    System.out.println(backWord.getBackWards());
+    System.out.println(backWord);
+
+    System.out.println();
+
+    backWord.setString("World");
+    System.out.println(backWord.getFirstChar());
+    System.out.println(backWord.getLastChar());
+    System.out.println(backWord.getBackWards());
+    System.out.println(backWord);
+
+    System.out.println();
+
+    backWord.setString("JukeBox");
+    System.out.println(backWord.getFirstChar());
+    System.out.println(backWord.getLastChar());
+    System.out.println(backWord.getBackWards());
+    System.out.println(backWord);
+
+    System.out.println();
+
+    backWord.setString("TCEA");
+    System.out.println(backWord.getFirstChar());
+    System.out.println(backWord.getLastChar());
+    System.out.println(backWord.getBackWards());
+    System.out.println(backWord);
+
+    System.out.println();
+
+    backWord.setString("UIL");
+    System.out.println(backWord.getFirstChar());
+    System.out.println(backWord.getLastChar());
+    System.out.println(backWord.getBackWards());
+    System.out.println(backWord);
 	}
 }
