48589e832fe9833dacce9f56a79f5f995ef0e287
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-27 12:50:06 -0800

    complete RaySumLast.java lab

---
+++ Unit-08/src/RaySumLast.java
@@ -0,0 +1,19 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Daniel Egorov 
+//Date - 27 Jan 2022
+
+public class RaySumLast {
+	public static int go(int[] ray) {
+    if (ray.length == 0) return -1;
+
+    int sum = 0;
+    int last = ray[ray.length - 1];
+    for (int i = 0; i < ray.length - 1; i++) {
+      if (ray[i] > last) sum += ray[i];
+    }
+    
+    if (sum == 0) return -1;
+    return sum;
+	}
+}
