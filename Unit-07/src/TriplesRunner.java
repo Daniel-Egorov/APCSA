0e27b9aeaf2cd9423c18c9bca3d716756adb1bc3
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-01-27 09:52:05 -0800

    complete Triples.java lab

---
+++ Unit-07/src/TriplesRunner.java
@@ -0,0 +1,11 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Daniel Egorov
+
+public class TriplesRunner {
+  public static void main(String args[]) {
+    Triples trip = new Triples();
+    trip.setNum(110);
+    System.out.println(trip);
+	}
+}
