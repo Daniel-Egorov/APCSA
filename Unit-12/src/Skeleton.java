f574d789a92f57c5cd69d92c8f5cb489b178b61a
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-04 09:00:50 -0800

    unit 12 template files

---
+++ Unit-12/src/Skeleton.java
@@ -0,0 +1,16 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Daniel Egorov
+
+import static java.lang.System.*; 
+
+public class Skeleton implements Monster
+{
+	//add instance variables	
+
+	//add a constructor
+
+	//add code to implement the Monster interface
+
+	//add a toString
+}
