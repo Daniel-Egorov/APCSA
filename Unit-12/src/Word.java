92c0e4e4526183f6debc5ceee6aed19c2526e5c2
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-07 08:09:57 -0800

    complete Word.java lab

--- Unit-12/src/Word.java
+++ Unit-12/src/Word.java
@@ -8,17 +8,21 @@ public class Word implements Comparable<Word>
 {
 	private String word;
 
-	public Word( String s )
-	{
+	public Word( String s ) {
+    this.word = s;
 	}
 
-	public int compareTo( Word rhs )
-	{		
+	public int compareTo( Word rhs ) {
+    if (this.word.length() < rhs.toString().length()) return -1;
+    else if (this.word.length() > rhs.toString().length()) return 1;
+    else {
+      if (this.word.compareTo(rhs.toString()) < 0) return -1;
+      else if (this.word.compareTo(rhs.toString()) > 0) return 1;
+    }
 		return 0;
 	}
 
-	public String toString()
-	{
-		return "";
+	public String toString() {
+		return this.word;
 	}
 }
--- Unit-12/src/WordRunner.java
+++ Unit-12/src/WordRunner.java
@@ -5,28 +5,42 @@
 import java.io.File; 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static java.lang.System.*;
 
 public class WordRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("words.dat"));
+		Scanner file = new Scanner(new File("/Users/eggnog/Developer/APCSA/Unit-12/src/words.dat"));
 
 		int size = file.nextInt();
 		file.nextLine();
-		
 
+    List<Word> sorted = new ArrayList<Word>();
 
+    for (int i = 0; i < size; i++) {
+      String word = file.next();
+      Word wordObject = new Word(word);
 
+      sorted.add(wordObject);
+    }
 
+    for (int i = 0; i < sorted.size(); i++) {
+      for (int j = 0; j < sorted.size() - 1; j++) {
+        if (sorted.get(j).compareTo(sorted.get(j + 1)) == 1) {
+          Word temp = sorted.get(j);
+          sorted.set(j, sorted.get(j + 1));
+          sorted.set(j + 1, temp);
+        }
+      }
+    }
 
-
-
-
-
-
-
+		for (int i = 0; i < sorted.size(); i++) {
+      System.out.println(sorted.get(i));
+    }
 	}
 }
