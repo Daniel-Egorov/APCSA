4ba5f167210f5737df95181ccc3bdc7b0490b482
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-11 08:29:05 -0800

    complete NumberSorter.java lab

--- Unit-13/src/NumberSorter.java
+++ Unit-13/src/NumberSorter.java
@@ -11,15 +11,39 @@ import java.util.Scanner;
 
 public class NumberSorter {
 
-  //instance variables and other methods not shown
-
   private static int getNumDigits(int number) {
     int count = 0;
+    while (number > 0) {
+      count++;
+      number /= 10;
+    } 
     return count;
   }
 
   public static int[] getSortedDigitArray(int number) {
-    int[] sorted = null;
+    int[] sorted = new int[NumberSorter.getNumDigits(number)];
+
+    for (int i = 0; i < sorted.length; i++) {
+      sorted[i] = number % 10;
+      number /= 10;
+    }
+
+    // quick sort
+    for (int i = 0; i < sorted.length; i++) {
+      int lowest = sorted[i];
+      int lowestIndex = i;
+      for (int j = i + 1; j < sorted.length; j++) {
+        if (sorted[j] < lowest) {
+          lowest = sorted[j];
+          lowestIndex = j;
+        }
+      }
+
+      // found lowest, now perform swap
+      sorted[lowestIndex] = sorted[i];
+      sorted[i] = lowest;
+    }
+    
     return sorted;
   }
 }
