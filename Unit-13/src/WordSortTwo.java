4ab2b3ee192e4d2ffaf61aa4672a8503188b56db
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-11 08:36:29 -0800

    complete WordSortTwo.java lab

--- Unit-13/src/WordSortTwo.java
+++ Unit-13/src/WordSortTwo.java
@@ -11,12 +11,30 @@ public class WordSortTwo {
 
   private String[] wordRay;
 
-  public WordSortTwo(String sentence) {}
+  public WordSortTwo(String sentence) {
+    this.wordRay = sentence.split(" ");
+  }
 
-  public void sort() {}
+  public void sort() {
+    for (int i = 0; i < this.wordRay.length; i++) {
+      String lowest = this.wordRay[i];
+      int lowestIndex = i;
+
+      for (int j = i + 1; j < this.wordRay.length; j++) {
+        if (this.wordRay[j].compareTo(lowest) < 0) {
+          lowest = this.wordRay[j];
+          lowestIndex = j;
+        }
+      }
+
+      this.wordRay[lowestIndex] = this.wordRay[i];
+      this.wordRay[i] = lowest;
+    }
+  }
 
   public String toString() {
     String output = "";
+    for (int i = 0; i < this.wordRay.length; i++) output += this.wordRay[i]  + "\n";
     return output + "\n\n";
   }
 }
--- Unit-13/src/WordSortTwoRunner.java
+++ Unit-13/src/WordSortTwoRunner.java
@@ -12,12 +12,15 @@ import java.util.Scanner;
 public class WordSortTwoRunner {
 
   public static void main(String args[]) throws IOException {
-    Scanner file = new Scanner(new File("wordsorttwo.dat"));
+    Scanner file = new Scanner(new File("/Users/eggnog/Developer/APCSA/Unit-13/src/wordsorttwo.dat"));
     int size = file.nextInt();
     file.nextLine();
     for (int i = 0; i < size; i++) {
       String sentence = file.nextLine();
       //instantiate a new WordSort
+      WordSortTwo wordSort = new WordSortTwo(sentence);
+      wordSort.sort();
+      System.out.println(wordSort);
     }
   }
 }
