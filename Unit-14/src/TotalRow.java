0c84aa00c01022c53aaa059aef1aa16ac6c67d39
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-14 08:56:29 -0700

    complete TotalRow.java lab

--- Unit-14/src/TotalRow.java
+++ Unit-14/src/TotalRow.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import java.util.*;
 
@@ -8,6 +8,14 @@ public class TotalRow {
 
   public static List<Integer> getRowTotals(int[][] m) {
     //add code here
-    return null;
+    List<Integer> result = new ArrayList<Integer>();
+    for (int i = 0; i < m.length; i++) {
+      int sum = 0;
+      for (int j = 0; j < m[i].length; j++) {
+        sum += m[i][j];
+      }
+      result.add(sum);
+    }
+    return result;
   }
 }
--- Unit-14/src/TotalRowRunner.java
+++ Unit-14/src/TotalRowRunner.java
@@ -1,10 +1,19 @@
+import javax.swing.ToolTipManager;
+
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 public class TotalRowRunner {
 
   public static void main(String args[]) throws Exception {
     //add code here
+    TotalRow runner = new TotalRow();
+    System.out.print("Row totals are : ");
+    System.out.println(TotalRow.getRowTotals(new int[][]{{1,2,3},{5,5,5,5}}));
+    System.out.print("Row totals are : ");
+    System.out.println(TotalRow.getRowTotals(new int[][]{{1,2,3},{5},{1},{2,2}}));
+    System.out.print("Row totals are : ");
+    System.out.println(TotalRow.getRowTotals(new int[][]{{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}}));
   }
 }
