1e8b964a10392e8b2dc062cf4d45b2c784fd1533
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-14 08:41:28 -0700

    complete RecursionFunOne.java lab

--- Unit-14/src/RecursionFunOne.java
+++ Unit-14/src/RecursionFunOne.java
@@ -1,12 +1,14 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*;
 
 public class RecursionFunOne {
 
   public static int countOddDigits(int num) {
-    return 0;
+    if (num == 0) return 0;
+    if ((num % 10) % 2 == 1) return 1 + countOddDigits(num / 10);
+    return countOddDigits(num / 10);
   }
 }
