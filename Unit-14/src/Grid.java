2aed5c2d6185db4881bca4e3e3ef9aa0275cf886
Daniel Egorov <daniel.s.egorov@gmail.com>
2022-03-14 09:45:11 -0700

    complete Grid.java lab

--- Unit-14/src/Grid.java
+++ Unit-14/src/Grid.java
@@ -1,27 +1,52 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 public class Grid {
 
   private String[][] grid;
 
   //load vals into the rows x cols grid randomly
-  public Grid(int rows, int cols, String[] vals) {}
+  public Grid(int rows, int cols, String[] vals) {
+    this.grid = new String[rows][cols];
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        int rand = (int)(Math.random() * vals.length);
+        this.grid[i][j] = vals[rand];
+      }
+    }
+  }
 
   //find out which of the vals occurs the most
   public String findMax(String[] vals) {
-    return "nothing yet";
+    String highest = vals[0];
+    for (String val : vals) {
+      if (this.countVals(val) > this.countVals(highest)) highest = val;
+    }
+    return highest + " occurs the most";
   }
 
   //returns a count of how many times val occurs in the matrix
   private int countVals(String val) {
-    return 0;
+    int count = 0;
+    for (int i = 0; i < this.grid.length; i++) {
+      for (int j = 0; j < this.grid[i].length; j++) {
+        if (this.grid[i][j].equals(val)) count++;
+      }
+    }
+    return count;
   }
 
   //display the grid
   public String toString() {
     String output = "";
+
+    for (int i = 0; i < this.grid.length; i++) {
+      for (int j = 0; j < this.grid[i].length; j++) {
+        output += this.grid[i][j] + " ";
+      }
+      output += "\n";
+    }
     return output;
   }
 }
--- Unit-14/src/GridRunner.java
+++ Unit-14/src/GridRunner.java
@@ -1,14 +1,26 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Daniel Egorov
 
 import static java.lang.System.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GridRunner {
 
-  public static void main(String args[]) throws IOException {}
+  public static void main(String args[]) throws IOException {
+    String[] vals = new String[]{"a", "b", "c", "x", "2", "7", "9"};
+    Grid grid = new Grid(20, 20, vals);
+    System.out.println(grid);
+    System.out.println(grid.findMax(vals));
+    
+    System.out.println();
+
+    grid = new Grid(10, 10, vals);
+    System.out.println(grid);
+    System.out.println(grid.findMax(vals));
+  }
 }
